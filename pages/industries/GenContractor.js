67b01ebd927e8375469a0a5de8a4400f4fe5ff6c
Sam <starullo87@gmail.com>
2024-08-17 21:51:46 -0500

    created genContracter and am using that as base for every industry/owner

--- pages/industries/GenContractor.js
+++ pages/industries/GenContractor.js
@@ -1,4 +1,18 @@
-const GenContractor = () =>
+export const GenContractor = () =>
 {
-    
+    const genCon = {
+        genInfo: {
+            indName: "General Contractor",
+            secondaryThing: "You know it's true",
+            money: "23 mil"
+        },
+        pictures: {
+            mainPic: null,
+            secondaryPic: undefined
+        },
+        deetz: {
+            genDeetz: ["Bid to award ratio", "whatever per whatever"],
+            services: ["Free consultation", "big time sexual healing"]
+        },
+    }
 }
